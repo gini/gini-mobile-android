28b34258390342cc7c821eecdf8c7515336d468a
Alpar Szotyori <alpar@gini.net>
2021-09-30 21:29:05 +0200

    ci: Add dokka configuration to buildSrc
    #PIA-1842

---
+++ buildSrc/src/main/kotlin/net/gini/gradle/DokkaConfig.kt
@@ -0,0 +1,42 @@
+package net.gini.gradle
+
+import com.android.build.gradle.LibraryExtension
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.get
+import org.gradle.kotlin.dsl.getByName
+import org.gradle.kotlin.dsl.getByType
+import org.jetbrains.dokka.gradle.DokkaTask
+
+class DokkaConfig: Plugin<Project> {
+
+    override fun apply(target: Project) {
+        target.plugins.apply("org.jetbrains.dokka")
+
+        target.dependencies {
+            add("dokkaHtmlPlugin", "org.jetbrains.dokka:kotlin-as-java-plugin:${Versions.Tools.dokka}")
+        }
+
+        target.tasks.getByName<DokkaTask>("dokkaHtml") {
+            outputDirectory.set(target.file("${target.buildDir}/docs/dokka"))
+            this.dokkaSourceSets.named("main").configure {
+                noAndroidSdkLink.set(false)
+
+                val library = target.extensions.getByType<LibraryExtension>()
+                val packageDocumentationPaths = library.sourceSets["main"].java.srcDirs.first()
+                    .walkTopDown()
+                    .filter { it.name == "package.md"}
+                    .map { it.absolutePath }
+                    .asIterable()
+
+                includes.from(target.files("module.md", packageDocumentationPaths))
+            }
+        }
+
+        target.tasks.getByName<DokkaTask>("dokkaJavadoc") {
+            outputDirectory.set(target.file("${target.buildDir}/docs/dokka-javadoc"))
+        }
+    }
+
+}
