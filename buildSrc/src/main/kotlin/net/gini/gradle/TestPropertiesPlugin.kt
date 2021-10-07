cb8a46bf80eb4fc258b5b1534f82a18687804fb9
Alpar Szotyori <alpar@gini.net>
2021-10-07 15:33:48 +0200

    ci: Add ability to create test properties from a map
    Replaces `injectTestProperties` task with `copyTestProperties` and
    repurposes `injectTestProperties` for creating the test properties
    from a map.

--- buildSrc/src/main/kotlin/net/gini/gradle/TestPropertiesPlugin.kt
+++ buildSrc/src/main/kotlin/net/gini/gradle/TestPropertiesPlugin.kt
@@ -1,39 +1,69 @@
 package net.gini.gradle
 
-import net.gini.gradle.TestPropertiesPluginExtension.Defaults
+import net.gini.gradle.extensions.mapProperty
+import net.gini.gradle.extensions.property
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.provider.MapProperty
 import org.gradle.api.provider.Property
 import org.gradle.api.tasks.Copy
+import org.gradle.api.tasks.WriteProperties
 import org.gradle.kotlin.dsl.create
 import org.gradle.kotlin.dsl.getByName
 import org.gradle.kotlin.dsl.register
-import java.io.File
+import java.time.ZonedDateTime
 
-interface TestPropertiesPluginExtension {
-    val source: Property<File>
-    val target: Property<File>
-
-    object Defaults {
-        const val sourceFile = "local.properties"
-        const val targetFile = "src/androidTest/assets/test.properties"
-    }
-}
-
+/**
+ * Created by Alpár Szotyori on 01.10.21.
+ *
+ * Copyright (c) 2021 Gini GmbH.
+ */
 class TestPropertiesPlugin : Plugin<Project> {
 
     override fun apply(target: Project) {
-        target.extensions.create<TestPropertiesPluginExtension>("testProperties")
+        target.extensions.create<TestPropertiesPluginExtension>("testProperties", target)
 
-        target.tasks.register<Copy>("injectTestProperties") {
+        target.tasks.register<Copy>("copyTestProperties") {
             val extension = target.extensions.getByName<TestPropertiesPluginExtension>("testProperties")
 
-            val sourceProperties = extension.source.orNull ?: target.file(Defaults.sourceFile)
-            val targetProperties = extension.target.orNull ?: target.file(Defaults.targetFile)
+            val sourceFile = target.file(extension.sourceFilePath.get())
+            val targetFile = target.file(extension.targetFilePath.get())
 
-            from(sourceProperties)
-            rename { targetProperties.name }
-            destinationDir = targetProperties.parentFile
+            from(sourceFile)
+            rename { targetFile.name }
+            destinationDir = targetFile.parentFile
         }
+
+        target.tasks.register<WriteProperties>("injectTestProperties") {
+            val extension = target.extensions.getByName<TestPropertiesPluginExtension>("testProperties")
+
+            outputFile = target.file(extension.targetFilePath.get())
+            encoding = "UTF-8"
+            comment = """
+                    DO NOT COMMIT THIS FILE TO GIT!
+    
+                    Automatically injected by injectTestProperties at ${ZonedDateTime.now()}.
+                """.trimIndent()
+
+            val properties = extension.properties.get()
+
+            if (properties.isEmpty()) {
+                throw IllegalArgumentException("No test properties found. Configure the TestPropertiesPluginExtension to add properties.")
+            }
+
+            properties(properties as Map<String, Any>)
+        }
+    }
+}
+
+open class TestPropertiesPluginExtension internal constructor(project: Project) {
+
+    val properties: MapProperty<String, String> = project.objects.mapProperty()
+
+    val sourceFilePath: Property<String> = project.objects.property {
+        set("local.properties")
+    }
+    val targetFilePath: Property<String> = project.objects.property {
+        set("src/androidTest/assets/test.properties")
     }
 }
