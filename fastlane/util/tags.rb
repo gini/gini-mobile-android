950504e4f584d4f6ff561d137a8146da93996f22
Alpar Szotyori <alpar@gini.net>
2021-10-13 14:43:38 +0200

    ci: Add fastlane with a lane for publishing to maven central

---
+++ fastlane/util/tags.rb
@@ -0,0 +1,51 @@
+##
+# Extract the project version from the tag.
+#
+# The tag must have the following format: `<project-id>;<version>`.
+#
+# Halts and prints an error if the project id is not in the tag.
+#
+def get_project_version(project_id, tag, ui)
+  tag = normalize_tag(tag)
+  components = tag.split(';')
+
+  if components[0] != project_id
+    ui.user_error! "The project id '#{project_id}' is not in the tag: #{tag}"
+  end
+
+  if components.size != 2
+    ui.user_error! "Missing project version from the tag: #{tag}"
+  end
+
+  components[1].strip
+end
+
+##
+# Extract the example app version of a project from the tag.
+#
+# The tag must have the following format: `<project-id>;<version>;<example-app-id>;<version>`.
+#
+# Halts and prints an error if the project id or example app id is not in the tag.
+#
+def get_example_app_version(project_id, example_app_id, tag, ui)
+  tag = normalize_tag(tag)
+  components = tag.split(';')
+
+  if components[0] != project_id
+    ui.user_error! "The project id '#{project_id}' is not in the tag: #{tag}"
+  end
+
+  if components[2] != example_app_id
+    ui.user_error! "The example app id '#{example_app_id}' is not in the tag: #{tag}"
+  end
+
+  if components.size != 4
+    ui.user_error! "Missing example app version from the tag: #{tag}"
+  end
+
+  components[3].strip
+end
+
+def normalize_tag(tag)
+  tag.delete_prefix("refs/tags/")
+end
