21fc41e6cc87bbff05b0fd8163a488abaf5a8b3f
Team Mobile <team-mobile@gini.net>
2021-10-19 13:22:18 +0000

    Release health-sdk documentation for tag refs/tags/health-sdk;0.0.2;doc-1

---
+++ health-sdk/dokka/scripts/sourceset_dependencies.js
@@ -0,0 +1,1 @@
+sourceset_dependencies='{":health-sdk:sdk:dokkaHtml/androidTestRelease":[],":health-sdk:sdk:dokkaHtml/debug":[],":health-sdk:sdk:dokkaHtml/main":[],":health-sdk:sdk:dokkaHtml/release":[]}'
