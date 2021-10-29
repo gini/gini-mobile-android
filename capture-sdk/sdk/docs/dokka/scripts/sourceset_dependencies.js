491e0f97ceafab5ff392faccb9b9834867782fbe
Team Mobile <team-mobile@gini.net>
2021-10-29 17:59:41 +0000

    Release capture-sdk documentation for tag refs/tags/capture-sdk;0.0.1;doc-1

--- capture-sdk/sdk/docs/dokka/scripts/sourceset_dependencies.js
+++
@@ -1,1 +0,0 @@
-sourceset_dependencies='{":capture-sdk:sdk:dokkaHtml/androidTestRelease":[],":capture-sdk:sdk:dokkaHtml/debug":[],":capture-sdk:sdk:dokkaHtml/main":[],":capture-sdk:sdk:dokkaHtml/release":[]}'
