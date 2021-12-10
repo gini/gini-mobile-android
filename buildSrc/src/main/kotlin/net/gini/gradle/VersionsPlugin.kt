1cb2f814a46431ab7c899d26906b60cf1207728b
Alpar Szotyori <alpar@gini.net>
2021-12-10 16:02:14 +0100

    ci: List outdated dependencies and projects grouped by dependency
    This affects the gradle task `dependencyUpdatesForAndroidProjects`.
    
    Makes it easier to determine which projects will be affected
    by each dependency update.

--- buildSrc/src/main/kotlin/net/gini/gradle/VersionsPlugin.kt
+++ buildSrc/src/main/kotlin/net/gini/gradle/VersionsPlugin.kt
@@ -1,10 +1,13 @@
 package net.gini.gradle
 
+import com.github.benmanes.gradle.versions.reporter.result.DependencyOutdated
+import com.github.benmanes.gradle.versions.reporter.result.Result
 import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+import net.gini.gradle.extensions.forEachAndroidProject
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.closureOf
 import org.gradle.kotlin.dsl.getByName
-import net.gini.gradle.extensions.forEachAndroidProject
 
 /**
  * Created by Alpár Szotyori on 11.11.21.
@@ -14,8 +17,38 @@ import net.gini.gradle.extensions.forEachAndroidProject
 
 class VersionsPlugin : Plugin<Project> {
 
+    private val outdatedDependenciesMap: MutableMap<String, Pair<DependencyOutdated, MutableSet<Project>>> =
+        mutableMapOf()
+
     override fun apply(target: Project) {
-        target.tasks.create("dependencyUpdatesForAndroidProjects")
+        val clearOutdatedDependenciesMapTask = target.tasks.create("clearOutdatedDependenciesMap") {
+            doFirst {
+                outdatedDependenciesMap.clear()
+            }
+        }
+        target.tasks.create("dependencyUpdatesForAndroidProjects") {
+            doLast {
+                logger.lifecycle(
+                    """
+                    ----------------------------------------
+                    Dependency Updates Grouped By Dependency
+                    ----------------------------------------
+                    
+                    The following dependencies have later milestone versions:
+                """.trimIndent()
+                )
+
+                outdatedDependenciesMap.forEach { (_, v) ->
+                    val (outdatedDependency, projects) = v
+                    logger.lifecycle(" - ${outdatedDependency.group}:${outdatedDependency.name} [${outdatedDependency.version} -> ${outdatedDependency.available.milestone}]")
+                    logger.lifecycle("   Projects:")
+                    projects.forEach { project ->
+                        logger.lifecycle("    - ${project.path}")
+                    }
+                }
+            }
+            dependsOn(clearOutdatedDependenciesMapTask)
+        }
 
         applyPluginToAllAndroidProjects(
             target = target,
@@ -30,8 +63,16 @@ class VersionsPlugin : Plugin<Project> {
 
             val dependencyUpdateTask =
                 androidProject.tasks.getByName<DependencyUpdatesTask>("dependencyUpdates").apply {
-                    outputDir = "${androidProject.buildDir}/dependencyUpdates"
                     rejectVersionIf { isNotSemver(candidate.version) }
+                    this.outputFormatter = closureOf<Result> {
+                        this.outdated.dependencies.forEach { outdatedDependency ->
+                            outdatedDependenciesMap[outdatedDependency.coordinate]?.second?.add(androidProject) ?: run {
+                                outdatedDependenciesMap[outdatedDependency.coordinate] =
+                                    outdatedDependency to mutableSetOf(androidProject)
+                            }
+                        }
+                    }
+
                 }
 
             target.tasks.getByName("dependencyUpdatesForAndroidProjects")
@@ -39,9 +80,12 @@ class VersionsPlugin : Plugin<Project> {
         }
     }
 
-    private fun isNotSemver(version: String): Boolean = !( semverRegex matches version)
+    private fun isNotSemver(version: String): Boolean = !(semverRegex matches version)
 
     companion object {
         private val semverRegex = """^[0-9]+\.[0-9]+\.[0-9]+$""".toRegex()
     }
 }
+
+private val DependencyOutdated.coordinate: String
+    get() = "$group:$name:$version"
