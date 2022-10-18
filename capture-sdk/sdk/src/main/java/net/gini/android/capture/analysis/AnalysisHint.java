dec7d0be1b6f207dcff8da1158a47f80707eaf8a
Levente Lorincz <levente.lorincz.ext@gini.net>
2022-10-18 12:00:36 +0300

    feat(capture-sdk): Analysis UI PR fixes

--- capture-sdk/sdk/src/main/java/net/gini/android/capture/analysis/AnalysisHint.java
+++ capture-sdk/sdk/src/main/java/net/gini/android/capture/analysis/AnalysisHint.java
@@ -12,11 +12,11 @@ import androidx.annotation.StringRes;
 
 enum AnalysisHint {
 
-    FLAT(R.drawable.gc_analysis_tip_icon, R.string.gc_photo_tip_flat, R.string.gc_photo_tip_flatten_the_page_title),
-    ALIGN(R.drawable.gc_analysis_tip_icon, R.string.gc_photo_tip_align, R.string.gc_photo_tip_align_title),
-    PARALLEL(R.drawable.gc_analysis_tip_icon, R.string.gc_photo_tip_parallel, R.string.gc_photo_tip_parallel_title),
-    LIGHTING(R.drawable.gc_analysis_tip_icon, R.string.gc_photo_tip_lighting, R.string.gc_photo_tip_good_lightning_title),
-    MULTIPAGE(R.drawable.gc_analysis_tip_icon, R.string.gc_photo_tip_multipage, R.string.gc_photo_tip_multiple_pages_title);
+    FLAT(R.drawable.gc_photo_tip_flat, R.string.gc_photo_tip_flat, R.string.gc_photo_tip_flatten_the_page_title),
+    ALIGN(R.drawable.gc_photo_tip_align, R.string.gc_photo_tip_align, R.string.gc_photo_tip_align_title),
+    PARALLEL(R.drawable.gc_photo_tip_parallel, R.string.gc_photo_tip_parallel, R.string.gc_photo_tip_parallel_title),
+    LIGHTING(R.drawable.gc_photo_tip_lighting, R.string.gc_photo_tip_lighting, R.string.gc_photo_tip_good_lightning_title),
+    MULTIPAGE(R.drawable.gc_photo_tip_multipage, R.string.gc_photo_tip_multipage, R.string.gc_photo_tip_multiple_pages_title);
 
     public int getDrawableResource() {
         return mDrawableResource;
