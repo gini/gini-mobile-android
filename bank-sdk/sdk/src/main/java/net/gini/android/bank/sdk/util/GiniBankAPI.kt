731175b5a9f788768ec4626fa8eb1e14fa3f0935
Alpar Szotyori <alpar@gini.net>
2022-10-23 14:17:20 +0200

    refactor(bank-sdk): Remove K prefix from new kotlin SessionManager class
    PIA-1298

--- bank-sdk/sdk/src/main/java/net/gini/android/bank/sdk/util/GiniBankAPI.kt
+++ bank-sdk/sdk/src/main/java/net/gini/android/bank/sdk/util/GiniBankAPI.kt
@@ -2,6 +2,7 @@ package net.gini.android.bank.sdk.util
 
 import android.content.Context
 import net.gini.android.bank.api.GiniBankAPI
+import net.gini.android.bank.api.GiniBankAPIBuilder
 import net.gini.android.core.api.authorization.SessionManager
 
 /**
@@ -15,5 +16,5 @@ fun getGiniApi(context: Context, clientId: String, clientSecret: String, emailDo
  * Minimal configuration for Gini Bank API
  */
 fun getGiniApi(context: Context, sessionManager: SessionManager): GiniBankAPI {
-    return GiniBankAPIBuilder(context, sessionManager).build()
+    return GiniBankAPIBuilder(context, sessionManager = sessionManager).build()
 }
