47a19be0452fe4c17da601f1914260640d767da3
Alpar Szotyori <alpar@gini.net>
2022-10-23 14:16:56 +0200

    refactor(health-sdk): Remove K prefix from new kotlin SessionManager class
    PIA-1298

--- health-sdk/sdk/src/main/java/net/gini/android/health/sdk/util/GiniHealthAPI.kt
+++ health-sdk/sdk/src/main/java/net/gini/android/health/sdk/util/GiniHealthAPI.kt
@@ -1,7 +1,7 @@
 package net.gini.android.health.sdk.util
 
 import android.content.Context
-import net.gini.android.core.api.authorization.KSessionManager
+import net.gini.android.core.api.authorization.SessionManager
 import net.gini.android.health.api.GiniHealthAPI
 import net.gini.android.health.api.GiniHealthAPIBuilder
 
@@ -20,6 +20,6 @@ fun getGiniApi(context: Context, clientId: String, clientSecret: String, emailDo
 /**
  * Minimal configuration for Gini API
  */
-fun getGiniApi(context: Context, sessionManager: KSessionManager): GiniHealthAPI {
+fun getGiniApi(context: Context, sessionManager: SessionManager): GiniHealthAPI {
     return GiniHealthAPIBuilder(context, sessionManager = sessionManager).build()
 }
