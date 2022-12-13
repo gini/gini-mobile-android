e6da151633addcf0f229c5b2983a03a5fe90b6e0
AdnanAppec <adnan@appec.nl>
2022-12-13 09:35:23 +0100

    feat(capture-sdk):
    - refactor docs for new created files
    
    PIA-2500

--- capture-sdk/sdk/src/main/java/net/gini/android/capture/Amount.kt
+++ capture-sdk/sdk/src/main/java/net/gini/android/capture/Amount.kt
@@ -5,17 +5,36 @@ import java.text.DecimalFormat
 import java.text.DecimalFormatSymbols
 import java.util.*
 
+/**
+ * This class serves for sending the extracted amount and currency in cleanup process
+ **/
+
+/**
+ * Creates an instance of Amount class
+ *
+ * @param mBigDecimal
+ * @param mCurrency
+ *
+ */
 class Amount(
     private val mBigDecimal: BigDecimal,
     private val mCurrency: AmountCurrency
 ) {
 
+    /**
+     * For internal use only
+     *
+     * @suppress
+     */
     fun amountToPay(): String {
         val decimalFormat = DecimalFormat("0.00", DecimalFormatSymbols.getInstance(Locale.ENGLISH))
         decimalFormat.isParseBigDecimal = true
         return decimalFormat.format(mBigDecimal) + ":" + mCurrency.name
     }
 
+    /**
+     * Creates thread safe static instance of Amount class
+     */
     companion object {
         @JvmStatic
         val EMPTY = Amount(BigDecimal.valueOf(0), AmountCurrency.EUR)
--- capture-sdk/sdk/src/main/java/net/gini/android/capture/AmountCurrency.kt
+++ capture-sdk/sdk/src/main/java/net/gini/android/capture/AmountCurrency.kt
@@ -1,5 +1,18 @@
 package net.gini.android.capture
 
+/**
+ * Enum class represents currently supported currencies
+ *
+ * <p>
+ * Currency codes expressed in ISO 4217 standard </p>
+ *
+ * <ul>
+ *  <li> EUR - stands for EURO (official currency of the euro-zone)
+ *  <li> GBP - stands for the British pound sterling
+ *  <li> USD - stands for United States Dollar
+ *  <li> CHF - stands for Confoederatio Helvetica franc
+ * </ul>
+ */
 enum class AmountCurrency {
     EUR, GBP, USD, CHF
 }
