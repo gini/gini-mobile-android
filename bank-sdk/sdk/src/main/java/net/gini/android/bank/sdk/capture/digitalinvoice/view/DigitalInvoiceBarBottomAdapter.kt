4f38e69943f36dbfc9f3d31fc3a902d2e3f01c55
AdnanAppec <yupicazin@gmail.com>
2023-01-23 17:43:27 +0100

    feat(bank-sdk):
    - Bottom injected container
    
    PIA-3553

---
+++ bank-sdk/sdk/src/main/java/net/gini/android/bank/sdk/capture/digitalinvoice/view/DigitalInvoiceBarBottomAdapter.kt
@@ -0,0 +1,35 @@
+package net.gini.android.bank.sdk.capture.digitalinvoice.view
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import net.gini.android.bank.sdk.databinding.GbsDigitalInvoiceNavigationBarBottomBinding
+import net.gini.android.capture.view.InjectedViewAdapter
+
+interface DigitalInvoiceBarBottomAdapter: InjectedViewAdapter {
+
+    fun setOnNextButtonClickListener(click: View.OnClickListener)
+}
+
+class DefaultDigitalInvoiceBarBottomAdapter: DigitalInvoiceBarBottomAdapter {
+
+    var viewBinding: GbsDigitalInvoiceNavigationBarBottomBinding? = null
+
+
+    override fun setOnNextButtonClickListener(click: View.OnClickListener) {
+        viewBinding?.gbsBarBottomNextButton?.setOnClickListener(click)
+    }
+
+    override fun onCreateView(container: ViewGroup): View {
+        val binding = GbsDigitalInvoiceNavigationBarBottomBinding.inflate(LayoutInflater.from(container.context), container, false)
+
+        viewBinding = binding
+
+        return viewBinding!!.root
+    }
+
+    override fun onDestroy() {
+        viewBinding = null
+    }
+
+}
