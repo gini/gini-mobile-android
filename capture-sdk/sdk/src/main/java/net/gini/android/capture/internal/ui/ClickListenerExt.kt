196afd66ac47c5ea01fc8312e90b86d46f55dea4
AdnanAppec <yupicazin@gmail.com>
2023-02-15 07:55:35 +0100

    feat(bank-sdk):
    - reference documentation
    
    PIA-3593

--- capture-sdk/sdk/src/main/java/net/gini/android/capture/internal/ui/ClickListenerExt.kt
+++ capture-sdk/sdk/src/main/java/net/gini/android/capture/internal/ui/ClickListenerExt.kt
@@ -3,6 +3,11 @@ package net.gini.android.capture.internal.ui
 import android.view.View
 import net.gini.android.capture.internal.ui.IntervalClickListener
 
+/**
+ * Internal use only.
+ *
+ * @suppress
+ */
 fun View.setIntervalClickListener(click: View.OnClickListener?) {
     setOnClickListener(IntervalClickListener(click))
 }
--- capture-sdk/sdk/src/main/java/net/gini/android/capture/internal/ui/IntervalToolbarMenuItemIntervalClickListener.kt
+++ capture-sdk/sdk/src/main/java/net/gini/android/capture/internal/ui/IntervalToolbarMenuItemIntervalClickListener.kt
@@ -5,6 +5,14 @@ import android.os.Looper
 import android.view.MenuItem
 import androidx.appcompat.widget.Toolbar
 
+/**
+ * Internal use only.
+ *
+ * Creates a enough delay between clicks on the Toolbar to prevent double taps
+ *
+ * @param itemListener
+ * @suppress
+ */
 class IntervalToolbarMenuItemIntervalClickListener(private val itemListener: Toolbar.OnMenuItemClickListener?): Toolbar.OnMenuItemClickListener,
     IntervalClickListenerHelper() {
 
