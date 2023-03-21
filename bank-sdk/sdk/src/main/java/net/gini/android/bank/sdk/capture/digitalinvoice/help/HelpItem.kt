eabdd38f0ac962c85e153f707458288504186ede
Levente Lorincz <levente.lorincz.ext@gini.net>
2023-03-21 09:20:17 +0200

    feat(bank-sdk): Changed help content descriptions

--- bank-sdk/sdk/src/main/java/net/gini/android/bank/sdk/capture/digitalinvoice/help/HelpItem.kt
+++ bank-sdk/sdk/src/main/java/net/gini/android/bank/sdk/capture/digitalinvoice/help/HelpItem.kt
@@ -9,12 +9,11 @@ import net.gini.android.bank.sdk.R
  * Custom enum class to gather all resources needed to show help tips.
  *
  */
-enum class HelpItem(@DrawableRes val drawableResource: Int, @StringRes val textResource: Int, @StringRes val titleTextResource: Int,
-                    @StringRes val iconContentTextResource: Int) {
+enum class HelpItem(@DrawableRes val drawableResource: Int, @StringRes val textResource: Int, @StringRes val titleTextResource: Int) {
     DIGITAL_INVOICE(R.drawable.gbs_help_question_icon, R.string.gbs_help_invoice_text,
-        R.string.gbs_help_invoice_title, R.string.gbs_digital_invoice_help_invoice_icon_content_description),
+        R.string.gbs_help_invoice_title),
     EDIT(R.drawable.gbs_help_edit_icon, R.string.gbs_help_edit_text,
-        R.string.gbs_help_edit_title, R.string.gbs_digital_invoice_help_edit_icon_content_description),
+        R.string.gbs_help_edit_title),
     SHOP(R.drawable.gbs_help_shop_icon, R.string.gbs_help_online_shops_text,
-        R.string.gbs_help_online_shops_title, R.string.gbs_digital_invoice_help_shop_icon_content_description)
+        R.string.gbs_help_online_shops_title)
 }
