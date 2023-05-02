48c1a06ce3038227c47dbc3c71fe9f8a5197ba1d
Alpar Szotyori <alpar@gini.net>
2023-05-02 15:17:30 +0200

    feat(capture-sdk): Find also nested IBANs and display only the IBAN
    Remove noise from the IBAN like spaces, dots, dashes and any text from before or after the IBAN.

--- capture-sdk/sdk/src/main/java/net/gini/android/capture/internal/ocr/IBANFilter.kt
+++ capture-sdk/sdk/src/main/java/net/gini/android/capture/internal/ocr/IBANFilter.kt
@@ -1,5 +1,7 @@
 package net.gini.android.capture.internal.ocr
 
+import android.graphics.Rect
+import android.util.Log
 import net.gini.android.capture.internal.qrcode.IBANValidator
 
 /**
@@ -11,14 +13,28 @@ class IBANFilter {
 
     private val ibanValidator: IBANValidator = IBANValidator()
 
+    private val ibanRegex = """[A-Z]{2}\d{2}.*\d""".toRegex()
+
     fun process(text: OCRText): OCRText {
-        return OCRText(text.elements.filter { element ->
-            try {
-                ibanValidator.validate(element.text)
-                true
-            } catch (ignore: Exception) {
-                false
+        return OCRText(text.elements.map { element ->
+            val filteredElement = element.text.filter { it.isLetterOrDigit() }
+            Log.d("filteredElement", filteredElement)
+            ibanRegex.find(filteredElement)?.let { firstMatchResult ->
+                var matchResult: MatchResult? = firstMatchResult
+                do {
+                    if (matchResult == null) {
+                        return@let
+                    }
+                    try {
+                        Log.d("matchResult", matchResult.value)
+                        ibanValidator.validate(matchResult.value)
+                        return@map OCRElement(matchResult.value, element.box)
+                    } catch (ignore: Exception) {
+                    }
+                    matchResult = matchResult.next()
+                } while (matchResult != null)
             }
-        })
+            OCRElement("", Rect())
+        }.filter { it.text.isNotBlank() })
     }
 }
