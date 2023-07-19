12ce2c4cebc36be1484a502af9c7b262d2826560
Mahdi Abolfazli <mahdi.abolfazli@gini.net>
2023-07-19 13:24:14 +0200

    feat(capture-sdk): Removed @HiltViewModel so the project builds
    PIA-4112

--- capture-sdk/screen-api-example-app/src/main/java/net/gini/android/capture/screen/ui/ConfigurationViewModel.kt
+++ capture-sdk/screen-api-example-app/src/main/java/net/gini/android/capture/screen/ui/ConfigurationViewModel.kt
@@ -1,8 +1,7 @@
 package net.gini.android.capture.screen.ui
 
 import androidx.lifecycle.ViewModel
-import dagger.hilt.android.lifecycle.HiltViewModel
 
-@HiltViewModel
+//@HiltViewModel
 class ConfigurationViewModel: ViewModel() {
 }
