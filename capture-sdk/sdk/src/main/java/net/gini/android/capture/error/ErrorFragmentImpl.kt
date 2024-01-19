424afec99a88eb97bcac93f7f93da6f92c8fbbb5
Alpar Szotyori <alpar@gini.net>
2024-01-19 18:54:49 +0100

    fix(capture-sdk): Bring back the top navigation bar for the error screen
    BSDK-258

--- capture-sdk/sdk/src/main/java/net/gini/android/capture/error/ErrorFragmentImpl.kt
+++ capture-sdk/sdk/src/main/java/net/gini/android/capture/error/ErrorFragmentImpl.kt
@@ -9,13 +9,19 @@ import android.widget.ImageView
 import android.widget.TextView
 import net.gini.android.capture.Document
 import net.gini.android.capture.EnterManuallyButtonListener
+import net.gini.android.capture.GiniCapture
 import net.gini.android.capture.R
 import net.gini.android.capture.document.ImageMultiPageDocument
 import net.gini.android.capture.internal.ui.FragmentImplCallback
+import net.gini.android.capture.internal.ui.IntervalClickListener
 import net.gini.android.capture.internal.ui.setIntervalClickListener
 import net.gini.android.capture.internal.util.ActivityHelper
 import net.gini.android.capture.tracking.AnalysisScreenEvent
 import net.gini.android.capture.tracking.EventTrackingHelper
+import net.gini.android.capture.view.InjectedViewAdapterHolder
+import net.gini.android.capture.view.InjectedViewContainer
+import net.gini.android.capture.view.NavButtonType
+import net.gini.android.capture.view.NavigationBarTopAdapter
 
 /**
  * Main logic implementation for error handling UI presented by {@link ErrorActivity}.
@@ -45,6 +51,8 @@ class ErrorFragmentImpl(
         val view: View = inflater.inflate(R.layout.gc_fragment_error, container, false)
         retakeImagesButton = view.findViewById(R.id.gc_button_error_retake_images)
 
+        setInjectedTopBarContainer(view)
+
         if (shouldAllowRetakeImages()) {
             retakeImagesButton.setIntervalClickListener {
                 EventTrackingHelper.trackAnalysisScreenEvent(AnalysisScreenEvent.RETRY)
@@ -61,7 +69,6 @@ class ErrorFragmentImpl(
             view.findViewById<TextView>(R.id.gc_error_header).text = it
         }
 
-
         errorType?.let {
             view.findViewById<TextView>(R.id.gc_error_header).text =
                 fragment.activity?.getString(it.titleTextResource)
@@ -72,6 +79,23 @@ class ErrorFragmentImpl(
         }
 
         return view
+    }
+
+    private fun setInjectedTopBarContainer(view: View) {
+        val topBarContainer =
+            view.findViewById<InjectedViewContainer<NavigationBarTopAdapter>>(R.id.gc_injected_navigation_bar_container_top)
+        if (GiniCapture.hasInstance()) {
+            topBarContainer.injectedViewAdapterHolder = InjectedViewAdapterHolder(GiniCapture.getInstance().internal().navigationBarTopAdapterInstance) { injectedViewAdapter ->
+                injectedViewAdapter.apply {
+                    setTitle(fragment.activity?.getString(R.string.gc_title_error) ?: "")
+
+                    setNavButtonType(NavButtonType.CLOSE)
+                    setOnNavButtonClickListener(IntervalClickListener {
+                        fragment.findNavController().popBackStack()
+                    })
+                }
+            }
+        }
     }
 
     fun setListener(enterManuallyButtonListener: EnterManuallyButtonListener?) {
