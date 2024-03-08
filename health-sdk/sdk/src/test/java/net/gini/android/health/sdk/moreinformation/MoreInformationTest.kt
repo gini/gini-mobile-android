d948a13d1e10909fc2332e7a7586ad9fad080637
dani <daniela.cretu@lateral-inc.com>
2024-03-08 13:38:39 +0200

    feat(health-sdk) Add Unit Tests for `MoreInformationViewModel`
    IPC-190

---
+++ health-sdk/sdk/src/test/java/net/gini/android/health/sdk/moreinformation/MoreInformationTest.kt
@@ -0,0 +1,10 @@
+package net.gini.android.health.sdk.moreinformation
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MoreInformationTest {
+
+
+}
