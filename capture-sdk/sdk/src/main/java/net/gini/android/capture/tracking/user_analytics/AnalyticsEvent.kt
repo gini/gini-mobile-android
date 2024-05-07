2c95214dc6e2078c0249e6f7a9349dcbb9fbe1fd
Mahdi Abolfazli <mahdi.abolfazli@gini.net>
2024-05-07 13:56:25 +0200

    refactor(capture-sdk): Refactor event package name and structure
    PP-438

--- capture-sdk/sdk/src/main/java/net/gini/android/capture/tracking/user_analytics/AnalyticsEvent.kt
+++ capture-sdk/sdk/src/main/java/net/gini/android/capture/tracking/user_analytics/AnalyticsEvent.kt
@@ -1,4 +1,4 @@
-package net.gini.android.capture.events
+package net.gini.android.capture.tracking.user_analytics
 
 
 enum class AnalyticsEvent(val eventName: String) {
--- capture-sdk/sdk/src/main/java/net/gini/android/capture/tracking/user_analytics/AnalyticsScreen.kt
+++ capture-sdk/sdk/src/main/java/net/gini/android/capture/tracking/user_analytics/AnalyticsScreen.kt
@@ -1,4 +1,4 @@
-package net.gini.android.capture.events
+package net.gini.android.capture.tracking.user_analytics
 
 enum class AnalyticsScreen(val screenName: String) {
     CAMERA("camera"),
