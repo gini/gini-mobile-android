ae34c24f1fdbab4f90d6aae9b5c5213919c0a25c
SyedaGinii <syeda.asad.ext@gini.net>
2024-09-13 12:05:16 +0300

    Added automation for No Result Screen
    PM-28

--- bank-sdk/example-app/src/androidTest/java/net/gini/android/bank/sdk/exampleapp/ui/testcases/NoResultTests.kt
+++ bank-sdk/example-app/src/androidTest/java/net/gini/android/bank/sdk/exampleapp/ui/testcases/NoResultTests.kt
@@ -10,6 +10,7 @@ import net.gini.android.bank.sdk.exampleapp.ui.resources.PdfUploader
 import net.gini.android.bank.sdk.exampleapp.ui.resources.SimpleIdlingResource
 import net.gini.android.bank.sdk.exampleapp.ui.screens.CaptureScreen
 import net.gini.android.bank.sdk.exampleapp.ui.screens.MainScreen
+import net.gini.android.bank.sdk.exampleapp.ui.screens.NoResultScreen
 import net.gini.android.bank.sdk.exampleapp.ui.screens.OnboardingScreen
 import net.gini.android.bank.sdk.exampleapp.ui.screens.ReviewScreen
 import org.junit.Before
@@ -17,11 +18,10 @@ import org.junit.Rule
 import org.junit.Test
 
 /**
- * Test class for Import PDF and Photos.
- *
- * No automation for step 4 and 5, since it is not part of our SDK
+ * Test class for No Result screen.
  */
-class ImportPdfImageTests {
+class NoResultScreenTests {
+
     @get:Rule
     val activityRule = activityScenarioRule<MainActivity>()
 
@@ -35,17 +35,17 @@ class ImportPdfImageTests {
     private val imageUploader = ImageUploader()
     private val pdfUploader = PdfUploader()
     private val reviewScreen = ReviewScreen()
+    private val noResultScreen = NoResultScreen()
     private lateinit var idlingResource: SimpleIdlingResource
-
 
     @Before
     fun setup() {
-        idlingResource = SimpleIdlingResource(2000)
+        idlingResource = SimpleIdlingResource(5000)
         IdlingRegistry.getInstance().register(idlingResource)
     }
 
     @Test
-    fun test1_uploadPhoto() {
+    fun test1_uploadInvalidImageAndClickEnterManuallyButton_NavigatesToMainScreen() {
         mainScreen.clickPhotoPaymentButton()
         onboardingScreen.clickSkipButton()
         captureScreen.clickFilesButton()
@@ -53,23 +53,39 @@ class ImportPdfImageTests {
         imageUploader.uploadImageFromPhotos()
         imageUploader.clickAddButton()
         idlingResource.waitForIdle()
-        reviewScreen.assertReviewTitleIsDisplayed()
-    }
-
-    @Test
-    fun test2_cancelUploadedPhoto() {
-        test1_uploadPhoto()
-        reviewScreen.clickCancelButton()
+        reviewScreen.clickProcessButton()
+        noResultScreen.checkNoResultTitleIsDisplay()
+        noResultScreen.checkNoResultHeaderIsDisplay()
+        noResultScreen.checkUsefulTipsIsDisplay()
+        noResultScreen.checkGoodLightingTitleIsDisplay()
+        noResultScreen.checkFlattenTitleIsDisplay()
+        noResultScreen.checkParallelTitleIsDisplay()
+        noResultScreen.checkPositionInTheFrameTitleIsDisplay()
+        noResultScreen.checkMultiPagesTitleIsDisplay()
+        idlingResource.waitForIdle()
+        noResultScreen.checkEnterManuallyButtonIsDisplay()
+        noResultScreen.clickEnterManuallyButton()
         mainScreen.assertDescriptionTitle()
+        idlingResource.waitForIdle()
     }
 
     @Test
-    fun test3_uploadPdf() {
+    fun test2_uploadInvalidPdfAndClickEnterManuallyButton_NavigatesToMainScreen() {
         mainScreen.clickPhotoPaymentButton()
         onboardingScreen.clickSkipButton()
         captureScreen.clickFilesButton()
         captureScreen.clickFiles()
-        pdfUploader.uploadPdfFromFiles("sample.pdf")
+        pdfUploader.uploadPdfFromFiles("test-pdf-no-results.pdf")
         idlingResource.waitForIdle()
+        noResultScreen.checkNoResultTitleIsDisplay()
+        noResultScreen.checkNoResultHeaderIsDisplay()
+        noResultScreen.checkSupportedFormatIsDisplay()
+        noResultScreen.checkComputerGeneratedInvoiceIsDisplayed()
+        noResultScreen.checkNotSupportedFormatIsDisplay()
+        noResultScreen.checkHandwritingIsDisplay()
+        idlingResource.waitForIdle()
+        noResultScreen.checkEnterManuallyButtonIsDisplay()
+        noResultScreen.clickEnterManuallyButton()
+        mainScreen.assertDescriptionTitle()
     }
 }
