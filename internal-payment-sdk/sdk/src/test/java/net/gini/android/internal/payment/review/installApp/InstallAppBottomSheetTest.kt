80ccffcda072ef4ff9dc411f1ec13213599a401b
Levente Lorincz <levente.lorincz.ext@gini.net>
2024-10-08 12:56:01 +0300

    refactor(health-sdk): Removed duplicate tests and fixed them in internal sdk

--- internal-payment-sdk/sdk/src/test/java/net/gini/android/internal/payment/review/installApp/InstallAppBottomSheetTest.kt
+++ internal-payment-sdk/sdk/src/test/java/net/gini/android/internal/payment/review/installApp/InstallAppBottomSheetTest.kt
@@ -1,9 +1,13 @@
 package net.gini.android.internal.payment.review.installApp
 
+import android.content.Context
 import android.view.View
 import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.common.truth.Truth
@@ -12,10 +16,14 @@ import io.mockk.mockk
 import io.mockk.verify
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.test.runTest
+import net.gini.android.health.api.GiniHealthAPI
+import net.gini.android.health.api.HealthApiDocumentManager
+import net.gini.android.internal.payment.GiniInternalPaymentModule
 import net.gini.android.internal.payment.R
 import net.gini.android.internal.payment.paymentComponent.PaymentComponent
 import net.gini.android.internal.payment.paymentComponent.SelectedPaymentProviderAppState
 import net.gini.android.internal.payment.paymentProvider.PaymentProviderApp
+import net.gini.android.internal.payment.utils.GiniLocalization
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -25,6 +33,9 @@ class InstallAppBottomSheetTest {
 
     private lateinit var paymentProviderApp: PaymentProviderApp
     private lateinit var paymentComponent: PaymentComponent
+    private var paymentComponentWithLocale: PaymentComponent? = null
+    private val giniHealthAPI: GiniHealthAPI = mockk(relaxed = true) { GiniHealthAPI::class.java }
+    private lateinit var giniPaymentModule: GiniInternalPaymentModule
 
     @Before
     fun setup() {
@@ -110,5 +121,63 @@ class InstallAppBottomSheetTest {
 
         // Then
         verify(exactly = 1) { listener.onForwardToBankSelected() }
+    }
+
+    @Test
+    fun `shows text values in english if that is set to GiniHealth`() = runTest {
+        // Given
+        val context: Context = ApplicationProvider.getApplicationContext()
+        giniPaymentModule = GiniInternalPaymentModule(context)
+        giniPaymentModule.setSDKLanguage(GiniLocalization.ENGLISH, context)
+        paymentComponentWithLocale = PaymentComponent(context, giniPaymentModule)
+        val documentManager: HealthApiDocumentManager = mockk { HealthApiDocumentManager::class.java }
+        every { giniHealthAPI.documentManager } returns documentManager
+
+
+        // When
+        val bottomSheet = InstallAppBottomSheet.newInstance(
+            paymentComponentWithLocale!!,
+            mockk(),
+            mockk(),
+            0
+        )
+
+        launchFragmentInContainer(themeResId = R.style.GiniPaymentTheme) {
+            bottomSheet
+        }
+
+        // Then
+        onView(withId(R.id.gps_install_app_title)).check(ViewAssertions.matches(ViewMatchers.withSubstring("Invoice")))
+        onView(withId(R.id.gps_install_app_details)).check(ViewAssertions.matches(ViewMatchers.withSubstring("Note:")))
+        onView(withId(R.id.gps_forward_button)).check(ViewAssertions.matches(ViewMatchers.withText("Forward")))
+    }
+
+    @Test
+    fun `shows text values in german if that is set to GiniHealth`() = runTest {
+        // Given
+        val context: Context = ApplicationProvider.getApplicationContext()
+        giniPaymentModule = GiniInternalPaymentModule(context)
+        giniPaymentModule.setSDKLanguage(GiniLocalization.GERMAN, context)
+        paymentComponentWithLocale = PaymentComponent(context, giniPaymentModule)
+        val documentManager: HealthApiDocumentManager = mockk { HealthApiDocumentManager::class.java }
+        every { giniHealthAPI.documentManager } returns documentManager
+
+
+        // When
+        val bottomSheet = InstallAppBottomSheet.newInstance(
+            paymentComponentWithLocale!!,
+            mockk(),
+            mockk(),
+            0
+        )
+
+        launchFragmentInContainer(themeResId = R.style.GiniPaymentTheme) {
+            bottomSheet
+        }
+
+        // Then
+        onView(withId(R.id.gps_install_app_title)).check(ViewAssertions.matches(ViewMatchers.withSubstring("Rechnungsdaten")))
+        onView(withId(R.id.gps_install_app_details)).check(ViewAssertions.matches(ViewMatchers.withSubstring("Hinweis:")))
+        onView(withId(R.id.gps_forward_button)).check(ViewAssertions.matches(ViewMatchers.withText("Weiter")))
     }
 }
