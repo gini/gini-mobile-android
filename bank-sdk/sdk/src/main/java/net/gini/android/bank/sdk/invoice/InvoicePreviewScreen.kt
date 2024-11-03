097e1809f9f07211c4f3ac7f5f3d04756b57a408
Niko <nickolai.dubkov@distcotech.com>
2024-11-03 21:09:34 +0100

    refactor(bank-sdk): Code refactor
    PP-881

--- bank-sdk/sdk/src/main/java/net/gini/android/bank/sdk/invoice/InvoicePreviewScreen.kt
+++ bank-sdk/sdk/src/main/java/net/gini/android/bank/sdk/invoice/InvoicePreviewScreen.kt
@@ -14,8 +14,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -28,7 +26,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
