da76c1183652a5ea58e21529199affbd04f28158
Levente Lorincz <levente.lorincz.ext@gini.net>
2024-12-09 09:23:54 +0200

    feat(internal-payment-sdk): Fixed alternative text
    IPC-407

--- internal-payment-sdk/sdk/src/main/java/net/gini/android/internal/payment/paymentComponent/PaymentComponentView.kt
+++ internal-payment-sdk/sdk/src/main/java/net/gini/android/internal/payment/paymentComponent/PaymentComponentView.kt
@@ -278,9 +278,10 @@ class PaymentComponentView(context: Context, attrs: AttributeSet?) : ConstraintL
             payInvoiceButton.text =
                 if (reviewFragmentWillBeShown) context.getString(R.string.gps_continue_to_overview)
                 else context.getString(R.string.gps_pay_button)
-            payInvoiceButton.contentDescription =
-                if (reviewFragmentWillBeShown) context.getString(R.string.gps_to_banking_app_content_description)
-                else context.getString(R.string.gps_to_banking_app_content_description)
+
+            if (reviewFragmentWillBeShown) {
+                payInvoiceButton.contentDescription = context.getString(R.string.gps_to_banking_app_content_description)
+            }
         }
     }
 
