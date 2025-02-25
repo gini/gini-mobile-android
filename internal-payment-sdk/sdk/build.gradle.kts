1249bab0af71eff00a8e6e35c6763c851621ae21
obaidgini <obaid.jatoi@gini.net>
2025-02-25 12:45:44 +0100

    refactor(internal-payment-sdk): added required testing library, and passed empty string instead of null as it is required now PM-137

--- internal-payment-sdk/sdk/build.gradle.kts
+++ internal-payment-sdk/sdk/build.gradle.kts
@@ -104,6 +104,7 @@ dependencies {
     testImplementation(libs.truth)
     testImplementation(libs.androidx.test.espresso.core)
     testImplementation(libs.androidx.test.espresso.intents)
+    androidTestImplementation(libs.androidx.test.junit)
 }
 
 apply<PublishToMavenPlugin>()
