ca8b18c98414fda7667e1660a11d188da8d03269
obaidgini <obaid.jatoi@gini.net>
2025-02-25 12:44:32 +0100

    refactor(bank-api-library): Removed extra parameter used PM-137

--- bank-api-library/library/src/test/java/net/gini/android/bank/api/BankApiDocumentRemoteSourceTest.kt
+++ bank-api-library/library/src/test/java/net/gini/android/bank/api/BankApiDocumentRemoteSourceTest.kt
@@ -120,8 +120,7 @@ class BankApiDocumentRemoteSourceTest {
                 null,
                 null,
                 null,
-                null,
-                null,
+                null
             ))
         }
 
