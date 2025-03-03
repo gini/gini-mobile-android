40f9b7ed3d3112b8878b3d20390d97720bf4206c
dani <daniela.cretu@lateral-inc.com>
2025-03-03 14:19:55 +0200

    feat(merchant-sdk): Added getting pdf name from string resources
    - Check if pdf name is valid, otherwise revert to default value
    
    IPC-592

--- merchant-sdk/sdk/src/main/java/net/gini/android/merchant/sdk/integratedFlow/PaymentFragment.kt
+++ merchant-sdk/sdk/src/main/java/net/gini/android/merchant/sdk/integratedFlow/PaymentFragment.kt
@@ -38,6 +38,7 @@ import net.gini.android.internal.payment.utils.PaymentNextStep
 import net.gini.android.internal.payment.utils.autoCleared
 import net.gini.android.internal.payment.utils.extensions.add
 import net.gini.android.internal.payment.utils.extensions.createShareWithPendingIntent
+import net.gini.android.internal.payment.utils.extensions.isValidPdfName
 import net.gini.android.internal.payment.utils.extensions.showInstallAppBottomSheet
 import net.gini.android.internal.payment.utils.extensions.showOpenWithBottomSheet
 import net.gini.android.internal.payment.utils.extensions.startSharePdfIntent
@@ -394,7 +395,9 @@ class PaymentFragment private constructor(
             paymentDetails = viewModel.paymentDetails,
             paymentRequestId = viewModel.paymentRequestFlow.value?.id ?: ""
         ) {
-            viewModel.onForwardToSharePdfTapped(requireContext().externalCacheDir)
+            val overriddenPdfName = getString(net.gini.android.internal.payment.R.string.gps_payment_request_pdf_name)
+            val pdfName = if (overriddenPdfName.isValidPdfName()) overriddenPdfName else getString(net.gini.android.internal.payment.R.string.gps_payment_request_pdf_name_default)
+            viewModel.onForwardToSharePdfTapped(requireContext().externalCacheDir, fileName = pdfName)
         }
         viewModel.addToBackStack(DisplayedScreen.OpenWithBottomSheet)
     }
