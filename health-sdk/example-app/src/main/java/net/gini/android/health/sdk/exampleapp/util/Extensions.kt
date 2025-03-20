f68f3a3bb9e75de8585e9f5baf6c4139301b54b0
mozhganpeivandiansharbaf <mozhgan.sharbaf@gini.net>
2025-03-20 08:17:20 +0100

    feat(health-sdk): add stubs bulk delete payment requests IPC-619

--- health-sdk/example-app/src/main/java/net/gini/android/health/sdk/exampleapp/util/Extensions.kt
+++ health-sdk/example-app/src/main/java/net/gini/android/health/sdk/exampleapp/util/Extensions.kt
@@ -1,7 +1,9 @@
 package net.gini.android.health.sdk.exampleapp.util
+import android.content.Context
 import android.icu.text.SimpleDateFormat
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import net.gini.android.health.sdk.exampleapp.R
@@ -41,6 +43,7 @@ fun String.prettifyDate(): String {
     }
 }
 
+@RequiresApi(Build.VERSION_CODES.N)
 fun String?.isInTheFuture(): Boolean {
     val format = SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss.SSS")
 
@@ -106,4 +109,12 @@ fun FragmentManager.add(fragment: Fragment) {
         .add(R.id.fragment_container, fragment, fragment::class.java.simpleName)
         .addToBackStack(fragment::class.java.simpleName)
         .commit()
+}
+
+fun Context.showAlertDialog(title: String, message: String) {
+    AlertDialog.Builder(this)
+        .setTitle(title)
+        .setMessage(message)
+        .setPositiveButton(android.R.string.ok, null)
+        .show()
 }
