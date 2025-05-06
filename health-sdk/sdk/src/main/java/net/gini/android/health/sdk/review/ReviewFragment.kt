0a3d44c7c76589148fcd43187daeb80267000d0a
mozhganpeivandiansharbaf <mozhgan.sharbaf@gini.net>
2025-05-06 12:17:04 +0200

    feat(health-sdk): fix indicator of pager landscape IPC-659

--- health-sdk/sdk/src/main/java/net/gini/android/health/sdk/review/ReviewFragment.kt
+++ health-sdk/sdk/src/main/java/net/gini/android/health/sdk/review/ReviewFragment.kt
@@ -186,8 +186,10 @@ class ReviewFragment private constructor(
         when (documentPagesResult) {
             is DocumentPagesResult.Success -> {
                 documentPageAdapter.submitList(documentPagesResult.pagesList.also { pages ->
-                    indicator.isVisible = pages.size > 1
+                    indicator.isVisible = true // Keep it visible to apply layout params
                     pager.isUserInputEnabled = pages.size > 1
+                    indicator.isEnabled = pages.size > 1
+                    indicator.alpha = if (pages.size > 1) 1f else 0f // visually hide it if needed
                 })
             }
             is DocumentPagesResult.Error -> {
