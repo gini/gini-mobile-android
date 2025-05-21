0915ff2999775dca1f8c7a681865f543a3852093
Niko <nickolai.dubkov.ext@gini.net>
2025-05-21 15:18:09 +0300

    feature(capture-sdk): QR Code education. Animation implementation
    PP-1126

--- capture-sdk/sdk/src/main/java/net/gini/android/capture/internal/camera/view/education/AnimatedEducationMessageWithIntro.kt
+++ capture-sdk/sdk/src/main/java/net/gini/android/capture/internal/camera/view/education/AnimatedEducationMessageWithIntro.kt
@@ -23,7 +23,6 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import net.gini.android.capture.R
 import net.gini.android.capture.ui.components.animation.animatedCharsLoadingIndicatorAsState
