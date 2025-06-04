edd535cb4dfb70fbacbfdafd3db1a77ab70ee9d4
obaidgini <obaid.jatoi@gini.net>
2025-06-04 11:23:46 +0200

    feat(capture-sdk): removing un used import PP-1385

--- capture-sdk/sdk/src/main/java/net/gini/android/capture/review/multipage/previews/PreviewPagesAdapter.kt
+++ capture-sdk/sdk/src/main/java/net/gini/android/capture/review/multipage/previews/PreviewPagesAdapter.kt
@@ -4,7 +4,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
-import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import net.gini.android.capture.AsyncCallback
 import net.gini.android.capture.GiniCapture
