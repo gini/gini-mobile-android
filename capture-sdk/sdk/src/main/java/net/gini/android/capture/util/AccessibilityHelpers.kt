80a34a3e913398eb5b63fa5600a4755babfebfc0
obaidgini <obaid.jatoi@gini.net>
2025-08-18 16:23:19 +0200

    feat(capture-sdk): Adding a helper method which will turn the date into correct format for talk back PP-1433

---
+++ capture-sdk/sdk/src/main/java/net/gini/android/capture/util/AccessibilityHelpers.kt
@@ -0,0 +1,46 @@
+package net.gini.android.capture.util
+
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
+
+private const val ST_KEY_WORD = "st"
+private const val ND_KEY_WORD = "nd"
+private const val RD_KEY_WORD = "rd"
+private const val TH_KEY_WORD = "th"
+
+/**
+ * TalkBack does not read the date correctly on some devices,
+ * for example we are using dd.MM.YYYY format, and if we select 01.09.2023,
+ * instead of reading "1st of September 2023", it reads "9 january 2023".
+ * That's why we need to set content description for the date field, with a spoken format which
+ * is constructed by this function [getSpokenDateForTalkBack]
+ * */
+
+fun getSpokenDateForTalkBack(date: String): String {
+
+    val inputFormat = SimpleDateFormat("dd.MM.yyyy", Locale.getDefault())
+    val parsedDate = inputFormat.parse(date) ?: return date
+
+    val locale = Locale.getDefault()
+
+    if (locale == Locale.ENGLISH) {
+        val calendar = Calendar.getInstance().apply { time = parsedDate }
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        val daySuffix = when (day) {
+            1, 21, 31 -> ST_KEY_WORD
+            2, 22 -> ND_KEY_WORD
+            3, 23 -> RD_KEY_WORD
+            else -> TH_KEY_WORD
+        }
+
+        val monthName = SimpleDateFormat("MMMM", locale).format(parsedDate)
+        val year = calendar.get(Calendar.YEAR)
+
+        return "$day$daySuffix of $monthName $year"
+    }
+    // This is for all the other locals, because we don't need suffix (1st, 3rd etc)
+    val spokenFormat = SimpleDateFormat("d. MMMM yyyy", locale)
+    return spokenFormat.format(parsedDate)
+}
