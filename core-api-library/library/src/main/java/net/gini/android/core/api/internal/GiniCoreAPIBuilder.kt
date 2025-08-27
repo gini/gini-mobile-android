ff57331ebba78585c104e80e0ca094472a11a14c
mozhganpeivandiansharbaf <mozhgan.sharbaf@gini.net>
2025-08-27 14:17:27 +0200

    fix(health-sdk): Disable debugging mode and clean up unused imports in ReviewView IPC-756

--- core-api-library/library/src/main/java/net/gini/android/core/api/internal/GiniCoreAPIBuilder.kt
+++ core-api-library/library/src/main/java/net/gini/android/core/api/internal/GiniCoreAPIBuilder.kt
@@ -70,7 +70,7 @@ abstract class GiniCoreAPIBuilder<DM : DocumentManager<DR, E>, G : GiniCoreAPI<D
     private var mUserRemoteSource: UserRemoteSource? = null
     private var mDocumentManager: DM? = null
     private var mDocumentRepository: DR? = null
-    private var isDebuggingEnabled = true
+    private var isDebuggingEnabled = false
     private val httpLoggingInterceptor: HttpLoggingInterceptor by lazy {
         HttpLoggingInterceptor().apply {
             level = HttpLoggingInterceptor.Level.BODY
