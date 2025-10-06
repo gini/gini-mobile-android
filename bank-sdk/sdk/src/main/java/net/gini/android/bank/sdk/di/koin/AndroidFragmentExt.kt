adc59121eac2312eb2166bb35871a2126d485bd8
Mahdi Abolfazli <mahdi.abolfazli@gini.net>
2025-10-06 16:38:12 +0200

    refactor(bank-sdk): Apply co-pilot comments on the PR
    PP-1725

--- bank-sdk/sdk/src/main/java/net/gini/android/bank/sdk/di/koin/AndroidFragmentExt.kt
+++ bank-sdk/sdk/src/main/java/net/gini/android/bank/sdk/di/koin/AndroidFragmentExt.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelStoreOwner
-import androidx.lifecycle.viewmodel.CreationExtras
 import net.gini.android.bank.sdk.di.getGiniBankKoin
 import org.koin.core.parameter.ParametersHolder
 import org.koin.core.qualifier.Qualifier
@@ -30,13 +29,12 @@ inline fun <reified T : ViewModel> Fragment.getGiniBankViewModel(
     val owner = ownerProducer()
 
     val factory = object : ViewModelProvider.Factory {
-        @Suppress("UNCHECKED_CAST")
-        override fun <T : ViewModel> create(modelClass: Class<T>, extras: CreationExtras): T {
-            return getGiniBankKoin().get<T>(
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            return getGiniBankKoin().get(
                 clazz = modelClass.kotlin,
                 qualifier = qualifier,
                 parameters = parameters
-            )
+            ) as T
         }
     }
 
