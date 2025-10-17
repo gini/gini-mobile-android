b3eadd4a17c0b2fe99bd3dfc6b0430dba111427c
obaidgini <obaid.jatoi@gini.net>
2025-10-17 12:45:08 +0200

    feat(capture-sdk): Configuring SDK flag in Capture PP-1727

--- capture-sdk/sdk/src/main/java/net/gini/android/capture/internal/provider/GiniBankConfigurationProvider.kt
+++ capture-sdk/sdk/src/main/java/net/gini/android/capture/internal/provider/GiniBankConfigurationProvider.kt
@@ -14,6 +14,7 @@ class GiniBankConfigurationProvider {
         isQrCodeEducationEnabled = false,
         isInstantPaymentEnabled = false,
         isEInvoiceEnabled = false,
+        savePhotosLocallyEnabled = false
     )
 
     fun provide(): Configuration = configuration
