46e5150f6ae83458d4e3a15d72eda3826e9f15b7
Mahdi Abolfazli <mahdi.abolfazli@gini.net>
2025-11-14 12:54:04 +0100

    docs(capture-sdk): Update KDoc in BankSDKBridge
    PP-1889

--- capture-sdk/sdk/src/main/java/net/gini/android/capture/BankSDKBridge.kt
+++ capture-sdk/sdk/src/main/java/net/gini/android/capture/BankSDKBridge.kt
@@ -2,18 +2,16 @@ package net.gini.android.capture
 
 /**
  * Internal use only.
- * <p>
- * Interface used by {@link CaptureFlowFragment} to dispatch Bank SDK properties to Capture SDK
- * </p>
+ *
+ * Interface used by [CaptureFlowFragment] to dispatch Bank SDK properties to Capture SDK.
  */
 interface BankSDKBridge {
 
     /**
-     * <p>
-     *     This method is implemented in CaptureFlowFragment to send the Bank SDK properties to the Capture SDK.
-     * </p>
+     * This method is implemented in `CaptureFlowFragment` to send the Bank SDK properties to the Capture SDK.
+     *
      * @param captureResult a success result from the Capture SDK
-     * @return BankSDKProperties returns the Bank SDK properties to be used in the Capture SDK
+     * @return the Bank SDK properties to be used in the Capture SDK
      */
     fun getBankSDKProperties(captureResult: CaptureSDKResult.Success): BankSDKProperties
 }
