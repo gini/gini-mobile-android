87b4ae95b7bce9f1264bc9fa32bbed9ccb7fb35b
Mahdi Abolfazli <mahdi.abolfazli@gini.net>
2025-12-11 14:51:11 +0100

    feat(capture-sdk): Add credit notes to WarningType enum
    PP-2180

--- capture-sdk/sdk/src/main/java/net/gini/android/capture/analysis/warning/WarningType.java
+++ capture-sdk/sdk/src/main/java/net/gini/android/capture/analysis/warning/WarningType.java
@@ -1,5 +1,6 @@
 package net.gini.android.capture.analysis.warning;
 
+import androidx.annotation.DrawableRes;
 import androidx.annotation.StringRes;
 
 import net.gini.android.capture.R;
@@ -13,16 +14,36 @@ import net.gini.android.capture.R;
 public enum WarningType {
     DOCUMENT_MARKED_AS_PAID(
             R.string.gc_document_marked_paid_title,
-            R.string.gc_document_marked_paid_desc
+            R.string.gc_document_marked_paid_desc,
+            R.drawable.gc_bg_warning_circle
+    ), DOCUMENT_MARKED_AS_CREDIT_NOTE(
+            R.string.gc_document_marked_credit_note_title,
+            R.string.gc_document_marked_credit_note_desc,
+            R.drawable.gc_warning_icon
     );
 
-    @StringRes private final int titleRes;
-    @StringRes private final int descriptionRes;
+    @StringRes
+    private final int titleRes;
+    @StringRes
+    private final int descriptionRes;
+    @DrawableRes
+    private final int iconRes;
 
-    WarningType(@StringRes int titleRes, @StringRes int descriptionRes) {
+    WarningType(@StringRes int titleRes, @StringRes int descriptionRes, @DrawableRes int iconRes) {
         this.titleRes = titleRes;
         this.descriptionRes = descriptionRes;
+        this.iconRes = iconRes;
     }
-    public int getTitleRes() { return titleRes; }
-    public int getDescriptionRes() { return descriptionRes; }
+
+    public int getTitleRes() {
+        return titleRes;
+    }
+
+    public int getDescriptionRes() {
+        return descriptionRes;
+    }
+
+    public int getIconRes() {
+        return iconRes;
+    }
 }
