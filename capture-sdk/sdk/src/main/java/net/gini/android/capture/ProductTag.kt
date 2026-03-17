da4af48bf21f552073668e792e8a82828cfe17c0
Qazi Naveed <qazi.naveed@gini.net>
2026-03-17 12:16:15 +0100

    feat(GiniCaptureSDK): Update properties with parcelize
    PP-2377

--- capture-sdk/sdk/src/main/java/net/gini/android/capture/ProductTag.kt
+++ capture-sdk/sdk/src/main/java/net/gini/android/capture/ProductTag.kt
@@ -16,11 +16,13 @@ sealed class ProductTag(val value: String) : Parcelable {
      * SEPA extractions - shows normal extractions.
      * This is the default behavior.
      */
+    @Parcelize
     object SepaExtractions : ProductTag("sepa-extractions")
 
     /**
      * Cross-border extractions - shows compound extractions.
      */
+    @Parcelize
     object CxExtractions : ProductTag("cx-extractions")
 
     /**
@@ -28,6 +30,7 @@ sealed class ProductTag(val value: String) : Parcelable {
      *
      * Note: This option is reserved for future use and is not yet available for customer use.
      */
+    @Parcelize
     object AutoDetectExtractions : ProductTag("auto-detect-extractions")
 
     /**
