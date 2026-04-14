a569884c37a001e05f56122155a20913b69d2565
Mahdi Abolfazli <abolfazli.mahdi@gmail.com>
2026-04-14 16:12:19 +0200

    fix(capture-sdk): Add flash availability check to CaptureScreenTests
    PP-2488

--- bank-sdk/example-app/src/androidTest/java/net/gini/android/bank/sdk/exampleapp/ui/testcases/CaptureScreenTests.kt
+++ bank-sdk/example-app/src/androidTest/java/net/gini/android/bank/sdk/exampleapp/ui/testcases/CaptureScreenTests.kt
@@ -1,10 +1,13 @@
 package net.gini.android.bank.sdk.exampleapp.ui.testcases
 
 import android.Manifest
+import android.content.Context
+import android.hardware.camera2.CameraCharacteristics
+import android.hardware.camera2.CameraManager
 import androidx.test.espresso.Espresso.pressBack
 import androidx.test.espresso.IdlingRegistry
 import androidx.test.ext.junit.rules.activityScenarioRule
-import androidx.test.filters.FlakyTest
+import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.GrantPermissionRule
 import net.gini.android.bank.sdk.exampleapp.ui.MainActivity
 import net.gini.android.bank.sdk.exampleapp.ui.resources.SimpleIdlingResource
@@ -12,6 +15,7 @@ import net.gini.android.bank.sdk.exampleapp.ui.screens.CaptureScreen
 import net.gini.android.bank.sdk.exampleapp.ui.screens.ConfigurationScreen
 import net.gini.android.bank.sdk.exampleapp.ui.screens.MainScreen
 import net.gini.android.bank.sdk.exampleapp.ui.screens.OnboardingScreen
+import org.junit.Assume.assumeTrue
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -39,9 +43,25 @@ class CaptureScreenTests {
         IdlingRegistry.getInstance().register(idlingResource)
     }
 
+    private fun assumeFlashAvailable() {
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        val cameraManager = context.getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        val hasFlash = try {
+            cameraManager.cameraIdList.any { id ->
+                val characteristics = cameraManager.getCameraCharacteristics(id)
+                characteristics.get(CameraCharacteristics.LENS_FACING) == CameraCharacteristics.LENS_FACING_BACK &&
+                        characteristics.get(CameraCharacteristics.FLASH_INFO_AVAILABLE) == true
+            }
+        } catch (e: Exception) {
+            false
+        }
+        assumeTrue("Skipping: device does not have camera flash hardware", hasFlash)
+    }
+
 
     @Test
     fun test1_flashIsOnByDefault() {
+        assumeFlashAvailable()
         mainScreen.clickSettingButton()
         configurationScreen.scrollToAnalysisText()
         configurationScreen.clickFlashToggleToEnable()
@@ -54,6 +74,7 @@ class CaptureScreenTests {
 
     @Test
     fun test2_flashEnabledWhenImageIsCaptured() {
+        assumeFlashAvailable()
         mainScreen.clickSettingButton()
         configurationScreen.scrollToAnalysisText()
         configurationScreen.clickFlashToggleToEnable()
@@ -66,6 +87,7 @@ class CaptureScreenTests {
 
     @Test
     fun test3_flashIsOffByDefault() {
+        assumeFlashAvailable()
         mainScreen.clickSettingButton()
         configurationScreen.scrollToAnalysisText()
         configurationScreen.assertFlashToggleIsDisable()
@@ -78,6 +100,7 @@ class CaptureScreenTests {
 
     @Test
     fun test4_flashDisabledWhenImageIsCaptured() {
+        assumeFlashAvailable()
         mainScreen.clickSettingButton()
         configurationScreen.scrollToAnalysisText()
         configurationScreen.assertFlashToggleIsDisable()
